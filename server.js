d4220877b3062ba2810b2ee606b3d97d0b5de260
Cory Zavod <crz5104@psu.edu>
2025-11-05 10:08:35 -0500

    server update

---
+++ server.js
@@ -0,0 +1,47 @@
+//console.log('Hello from Node!!')
+
+const http = require('http')
+
+const hostname = '127.0.0.1'
+
+const port = 3000
+
+const server = http.createServer((req,res) => {
+res.statusCode = 200
+res.setHeader('Content-type','text/plain')
+res.setHeader('Access-Control-Allow-Origin','*')
+
+// const method = req.method
+// const url = req.url
+
+const {method, url} = req
+
+
+
+// res.write(`Welcome to our web server!`)
+// res.write(`Method: ${method}, URL: ${url}`)
+
+if (url === '/getcat' && method === 'GET')
+{
+res.write(`Here is a cat!`)
+}
+else if (url === '/getdog' && method === 'GET')
+{
+res.write(`Here is a dog!`)
+}
+else if (url === '/additem' && method === 'PUT')
+{
+res.write(`System updated.`)
+}
+else
+{
+res.write(`I don't recognize that request.`)
+}
+
+res.end()
+}
+)
+
+server.listen(port, hostname, () => {
+console.log(`Server running at ${hostname}:${port}.`)
+})
